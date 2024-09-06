d5fa18961e605a872bc1d2ffb88534632a4e221f
Luffy Liu <muranhuli@gmail.com>
2024-09-06 15:17:36 +0800

    Improve: 新形式的超点利用LSH加速方法

--- hnswlib/utils.h
+++ hnswlib/utils.h
@@ -10,17 +10,19 @@
 #include <iostream>
 #include <memory>
 #include <H5Cpp.h>
+#include <vector>
+#include <random>
 
 class Time
 {
 private:
-    std::string description;  // 函数名
-    std::string timeUnit;  // 时间单位
+    std::string description; // 函数名
+    std::string timeUnit; // 时间单位
     std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
 
 public:
-    Time(const std::string &name, const std::string &unit = "s") :
-            description(std::move(name)), timeUnit(std::move(unit))
+    explicit Time(const std::string& name, const std::string& unit = "s") :
+        description(std::move(name)), timeUnit(std::move(unit))
     {
         start = std::chrono::high_resolution_clock::now();
     }
@@ -59,10 +61,10 @@ class DataRead
 {
 public:
     static std::unique_ptr<float[]>
-    read_hdf5_float(const std::string &filename, const std::string &dataset_name, hsize_t *dims_out)
+    read_hdf5_float(const std::string& filename, const std::string& dataset_name, hsize_t* dims_out)
     {
-        const H5std_string &FILE_NAME(filename);
-        const H5std_string &TRAIN_DATASET_NAME(dataset_name);
+        const H5std_string& FILE_NAME(filename);
+        const H5std_string& TRAIN_DATASET_NAME(dataset_name);
         H5::H5File file(FILE_NAME, H5F_ACC_RDONLY);
         H5::DataSet dataset = file.openDataSet(TRAIN_DATASET_NAME);
         H5::DataSpace dataspace = dataset.getSpace();
@@ -80,10 +82,10 @@ public:
     }
 
     static std::unique_ptr<int[]>
-    read_hdf5_int(const std::string &filename, const std::string &dataset_name, hsize_t *dims_out)
+    read_hdf5_int(const std::string& filename, const std::string& dataset_name, hsize_t* dims_out)
     {
-        const H5std_string &FILE_NAME(filename);
-        const H5std_string &TRAIN_DATASET_NAME(dataset_name);
+        const H5std_string& FILE_NAME(filename);
+        const H5std_string& TRAIN_DATASET_NAME(dataset_name);
         H5::H5File file(FILE_NAME, H5F_ACC_RDONLY);
         H5::DataSet dataset = file.openDataSet(TRAIN_DATASET_NAME);
         H5::DataSpace dataspace = dataset.getSpace();
@@ -101,40 +103,84 @@ public:
     }
 };
 
-void schedule(const std::string &content, int i, int max_elements)
+void schedule(const std::string& content, int i, int max_elements)
 {
     if (i == max_elements - 1)
-        std::cout << "\r"<<content<<" " << i << "/" << max_elements <<std::endl;
+        std::cout << "\r" << content << " " << i << "/" << max_elements << std::endl;
     else
-        std::cout << "\r"<<content<<" " << i << "/" << max_elements << std::flush;
+        std::cout << "\r" << content << " " << i << "/" << max_elements << std::flush;
 }
 
-class CounterSingleton {
+class CounterSingleton
+{
 public:
-    static CounterSingleton& getInstance() {
+    static CounterSingleton& getInstance()
+    {
         static CounterSingleton instance;
         return instance;
     }
 
-    void increment() {
+    void increment()
+    {
         count++;
     }
 
     // 清空计数器
-    void clear() {
+    void clear()
+    {
         count = 0;
     }
 
-    int getCount() const {
+    int getCount() const
+    {
         return count;
     }
 
 private:
     int count;
 
-    CounterSingleton() : count(0) {}
+    CounterSingleton() : count(0)
+    {
+    }
+
     CounterSingleton(const CounterSingleton&) = delete;
     CounterSingleton& operator=(const CounterSingleton&) = delete;
+};
+
+
+// 方法：https://blog.csdn.net/coffee_cream/article/details/109146143
+class LSH
+{
+private:
+    std::vector<float> r;
+    float b;
+    double w;
+    size_t dimension;
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution;
+
+public:
+    LSH(size_t dim, double width, unsigned seed = 42) : dimension(dim), w(width), distribution(0.0, 1.0),
+                                                        generator(seed)
+    {
+        // 初始化随机向量r
+        for (int i = 0; i < dimension; ++i)
+        {
+            r.push_back(distribution(generator));
+        }
+        // 随机选择偏移量b，范围在0到w之间
+        b = (distribution(generator) * w) / 2.0;
+    }
+
+    int getHashValue(const void* v) const
+    {
+        double dotProduct = 0.0;
+        for (int i = 0; i < dimension; ++i)
+        {
+            dotProduct += r[i] * ((float*)v)[i];
+        }
+        return static_cast<int>((dotProduct + b) / w);
+    }
 };
 
 
